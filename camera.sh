adf393428af975d59f0e90227858a17920a29d23
Keisuke Hirota <tahiro.k.ad@gmail.com>
2020-05-05 22:49:54 +0900

    Bug fix

--- camera.sh
+++ camera.sh
@@ -11,12 +11,12 @@ source "$(cd $(dirname "$0") && pwd)/variables.conf"
 function slack::upload_file() {
   local file="$1"
   local response="$(curl -fsL \
-                         -F 'token=${SLACK_OAUTH_TOKEN}' \
-                         -F 'channels=${SLACK_CHANNEL_ID}' \
-                         -F 'file=@${file}' \
-                         -F 'filename=${UNIX_TIME}.jpg' \
-                         -F 'initial_comment=${TIMESTAMP}' \
-                         '${SLACK_FILE_UPLOAD_URL}')"
+                         -F \"token=${SLACK_OAUTH_TOKEN}\" \
+                         -F \"channels=${SLACK_CHANNEL_ID}\" \
+                         -F \"file=@${file}\" \
+                         -F \"filename=${UNIX_TIME}.jpg\" \
+                         -F \"initial_comment=${TIMESTAMP}\" \
+                         \"${SLACK_FILE_UPLOAD_URL}\")"
   echo "${response}" | python -c 'import sys; import json; sys.exit(0 if json.loads(raw_input())["ok"] else 1)'
 }
 
@@ -40,7 +40,7 @@ function shell::ok() {
   rm -f "${TMPFILE}"
   echo "camera.sh has successed."
 
-  sudo shutdown -h now
+  # sudo shutdown -h now
 }
 
 function shell::ng() {
@@ -48,7 +48,7 @@ function shell::ng() {
   slack::post "${text}"
   echo "${text}" 1>&2
   
-  sudo shutdown -h now
+  # sudo shutdown -h now
 }
 
 function main() {

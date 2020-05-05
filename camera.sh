d1d38c154a2c9698eff1ec9e35f884d0184532b7
Keisuke Hirota <tahiro.k.ad@gmail.com>
2020-05-05 22:42:31 +0900

    Add response analysis

--- camera.sh
+++ camera.sh
@@ -10,13 +10,14 @@ source "$(cd $(dirname "$0") && pwd)/variables.conf"
 
 function slack::upload_file() {
   local file="$1"
-  curl -fsL \
-       -F "token=${SLACK_OAUTH_TOKEN}" \
-       -F "channels=${SLACK_CHANNEL_ID}" \
-       -F "file=@${file}" \
-       -F "filename=${UNIX_TIME}.jpg" \
-       -F "initial_comment=${TIMESTAMP}" \
-       "${SLACK_FILE_UPLOAD_URL}" >/dev/null
+  local response="$(curl -fsL \
+                         -F 'token=${SLACK_OAUTH_TOKEN}' \
+                         -F 'channels=${SLACK_CHANNEL_ID}' \
+                         -F 'file=@${file}' \
+                         -F 'filename=${UNIX_TIME}.jpg' \
+                         -F 'initial_comment=${TIMESTAMP}' \
+                         '${SLACK_FILE_UPLOAD_URL}')"
+  echo "${response}" | python -c 'import sys; import json; sys.exit(0 if json.loads(raw_input())["ok"] else 1)'
 }
 
 function slack::create_payload() {

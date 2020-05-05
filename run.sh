9cbfa1f772a08a1d598c810730878c6efdb3db04
Keisuke Hirota <tahiro.k.ad@gmail.com>
2020-05-05 18:47:36 +0900

    Add schedule update

--- run.sh
+++ run.sh
@@ -9,8 +9,21 @@ function git::pull() {
   popd
 }
 
+function witty::update() {
+  expect -c "
+  spawn \"${HOME}/wittypi/wittyPi.sh\"
+  expect \"What do you want to do? (1~11)\"
+  send \"6\n\"
+  expect \"Which schedule script do you want to use? (1~7)\"
+  send \"6\n\"
+  expect \"What do you want to do? (1~11)\"
+  send \"11\n\"
+  "
+}
+
 function main() {
   git::pull
+  witty::update
   "${SRC_DIR}/camera.sh"
 }
 

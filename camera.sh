b24eb4af814cc3effc81b035050abefea0cb7433
Keisuke Hirota <tahiro.k.ad@gmail.com>
2020-05-05 22:55:43 +0900

    Bug fix

--- camera.sh
+++ camera.sh
@@ -11,12 +11,12 @@ source "$(cd $(dirname "$0") && pwd)/variables.conf"
 function slack::upload_file() {
   local file="$1"
   local response="$(curl -fsL \
-                         -F \"token=${SLACK_OAUTH_TOKEN}\" \
-                         -F \"channels=${SLACK_CHANNEL_ID}\" \
-                         -F \"file=@${file}\" \
-                         -F \"filename=${UNIX_TIME}.jpg\" \
-                         -F \"initial_comment=${TIMESTAMP}\" \
-                         \"${SLACK_FILE_UPLOAD_URL}\")"
+                         -F "token=${SLACK_OAUTH_TOKEN}" \
+                         -F "channels=${SLACK_CHANNEL_ID}" \
+                         -F "file=@${file}" \
+                         -F "filename=${UNIX_TIME}.jpg" \
+                         -F "initial_comment=${TIMESTAMP}" \
+                         "${SLACK_FILE_UPLOAD_URL}")"
   echo "${response}" | python -c 'import sys; import json; sys.exit(0 if json.loads(raw_input())["ok"] else 1)'
 }
 
